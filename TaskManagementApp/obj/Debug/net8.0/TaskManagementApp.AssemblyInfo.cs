9517afecb6f17fc136db3692fd454119a4cb7ac7
shafakyildiz <shafakyildiz@gmail.com>
2024-02-01 13:54:56 +0300

    add task model

--- TaskManagementApp/obj/Debug/net8.0/TaskManagementApp.AssemblyInfo.cs
+++ TaskManagementApp/obj/Debug/net8.0/TaskManagementApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4b5b0cffb672565be7decb0babe4f4caeda040d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
